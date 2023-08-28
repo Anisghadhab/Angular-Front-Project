73f989bc822f2e74fb71b231001cf3f899180a75
Anisghadhab <anisghadhab@utctunisie.com>
2023-08-28 11:58:56 +0100

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './doctor/app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ConsultationFormComponent } from './consultation/consultation-form.component';
+import { PatientComponent } from './patient/patient.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConsultationFormComponent,
+    PatientComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule, // Add this line
+
   ],
   providers: [],
   bootstrap: [AppComponent]
